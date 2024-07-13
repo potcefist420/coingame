6ed01bd22188b5a91e4de5d288c6ed821142ead6
potcefist420 <127909894+potcefist420@users.noreply.github.com>
2024-07-13 23:51:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@
             height: 100vh;
             margin: 0;
             font-family: Arial, sans-serif;
-            background-color: #f0f0f0;
+            background-color: #892DD6;
         }
         #coinButton {
             width: 100px;
